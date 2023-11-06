516028523d0d35fd2af23539129ef154290ccba5
paradoxo <vinicius_baggio@hotmail.com>
2023-11-06 18:48:50 -0300

    refactor: React routes and match props for Page

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,21 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import {
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { RouteComponentProps, useParams } from "react-router";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Page.css";
 
-const Page: React.FC = () => {
-
-  const { name } = useParams<{ name: string; }>();
+const Page: React.FC<RouteComponentProps> = ({ match }) => {
+  const { name } = useParams<{ name: string }>();
+  console.log(`name: ${name}`);
+  console.log(`match:`);
+  console.log(match);
 
   return (
     <IonPage>
